095636d9dc95bc4f1acd2b89aa4e632741b7222d
Ze Gao <ze_gao@163.com>
2016-08-15 14:30:42 +0800

    fix prev show bug by adding extra filter to images prev and next selector

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,11 +4,11 @@ class Image < ActiveRecord::Base
 	has_many :admins, :through => :votes
 
 	def previous
-      Image.where(" id < ?",  self.id).last
+      Image.where(" id < ?",  self.id).where("flag = ?", false).last
     end
   
     def next
-      Image.where(" id > ?",  self.id).first
+      Image.where(" id > ?",  self.id).where("flag = ?", false).first
     end
     
     # def get_context
